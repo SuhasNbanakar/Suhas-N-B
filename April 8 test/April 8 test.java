4ff9dbcb4229122585989dc826aefb3ea2c6ca25
unknown <suhasnb.xworkz@gmail.com>
2022-04-08 14:31:12 +0530

    April 8 Test

---
+++ April 8 test/April 8 test.java
@@ -0,0 +1,113 @@
+1) 
+2) two type of datatypes in java.
+	* Primitive
+	* non-Primitive
+3)	
+4) java naming conventions is to start with a lowercase letter and next new word start with uppercase(camel case).
+5)	Initialization is variable an initial the value to point. 
+	assignment a variable value to some point and creat the variable.
+6)	Array is a group of collection to same data type.
+7) no con't pass the negative number in array.
+8) Array stored in constantpool in jvm memory.
+9)	Advantages of Array
+	* array use to store a same datatypes of elements.
+	* array number of variable name store in single name .
+	* array store a n number elements.
+10)	
+11)	class Array{
+  public static void main(String[] agrs){
+  int[] value={1,3,6,7,8,4,9};
+  System.out.println(value[0]);
+  System.out.println(value[1]);
+  System.out.println(value[2]);
+  System.out.println(value[3]);
+  System.out.println(value[4]);
+  System.out.println(value[5]);
+  System.out.println(value[6]);
+
+  }
+}
+	
+12)	class ArraySort{
+	public static void main(String[] agrs){
+	 static int[] doNo={101,102,104,105,107,108};
+	 
+	 for(int i=0;i<ArraySort.length;i++)
+	 {
+	 System.out.println("dono "+i);
+	 
+	 }
+	
+	
+	
+	}
+
+
+
+
+}	
+13)
+14)	class ArrayException{
+	public static void main(String[] agrs)
+	{
+	String[] age={16,17,18};
+	
+	for(int i=0;i<6;i++)
+	{
+	System.out.prtintln("age"i++);
+	}
+	}
+}
+15)	variable Name might not have been initialized
+16) class ArraySL{
+	public static void main(String[] agrs)
+	{
+		int[] value={2.4.8.10};
+		
+		if(value<4)
+		{
+		System.out.println("smallest value"+value);
+		}
+		if(value>8)
+		{
+		System.out.println("largest value "+value);
+		}
+	}
+}
+}
+
+17)	static keyword = 
+	static variable=
+	static method=
+18)	overload is use a same datatypes in static method
+19) static is keyword to use a main method, jvm can invoke the values.
+20)	`
+21)	constructors is not a static in java.
+22)
+23) constructor chaining is one constructor calling a onther constructor.
+24) constructor is specil method to use new keyword. 
+25) 
+26)
+27) class Child{
+	string parentName;
+	
+	Child(String parentName)
+	{
+		Child.parentName=parentName;
+	}
+}
+class ChildRunner{
+public static void main(String[] agrs){
+     Child child=new Child("Nagaraj");
+	System.out.println(Child.parentName);
+}
+}
+28)instance is copy of a calss and use number copy use in new keyword
+	class is not use any  new keyword and not copy .
+29)instance is use a number ways to created a to use new keyword.
+30)instance is creat a new copy of class. 	
+
+
+
+
+ 
