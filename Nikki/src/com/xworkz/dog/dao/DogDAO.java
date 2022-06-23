b6e4934bfc864b50f7d81f4cb6f3d39f72429c2d
unknown <suhasnb.xworkz@gmail.com>
2022-06-24 00:53:08 +0530

    jdbc

--- Nikki/src/com/xworkz/dog/dao/DogDAO.java
+++ Nikki/src/com/xworkz/dog/dao/DogDAO.java
@@ -1,9 +1,15 @@
 package com.xworkz.dog.dao;
 
+import java.util.Collection;
+
 import com.xworkz.dog.dto.DogDTO;
 
 public interface DogDAO {
 
 	boolean save(DogDTO dogDTO);
+
+	default void save(Collection<DogDTO> dto) {
+
+	}
 
 }
